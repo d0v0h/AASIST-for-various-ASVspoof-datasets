49dc422f7f9fb58a52e40a6f63a2dadd98c99137
Impress_kdh <d0_0h@inu.ac.kr>
2025-09-01 19:13:57 +0900

    Add: ASVspoof 2021 LA & DF evaluation

---
+++ data_loaders/loader_2021.py
@@ -0,0 +1,208 @@
+import numpy as np
+import soundfile as sf
+import torch
+from torch import Tensor
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from typing import List, Dict
+from pathlib import Path
+
+from utils import seed_worker
+
+
+
+___author__ = "Hemlata Tak, Jee-weon Jung"
+__email__ = "tak@eurecom.fr, jeeweon.jung@navercorp.com"
+
+
+def get_database_path(database_path: Path, track=None) -> Dict[str, Path]:
+    # Return ASVspoof 2019 database path
+    ASVspoof_2019_path = database_path / 'ASVspoof2019'
+    ASVspoof_2021_path = database_path / 'ASVspoof2021'
+
+    paths = {}
+
+    # write flac path
+    paths["trn_flac_path"] = ASVspoof_2019_path / "ASVspoof2019_LA_train"
+    paths["dev_flac_path"] = ASVspoof_2019_path / "ASVspoof2019_LA_dev"
+    paths["eval_flac_path"] = ASVspoof_2021_path / f"ASVspoof2021_{track}_eval"
+
+    # write meta path
+    paths["trn_meta_path"] = (ASVspoof_2019_path / "ASVspoof2019_LA_cm_protocols" / "ASVspoof2019.LA.cm.train.trn.txt")
+    paths["dev_meta_path"] = (ASVspoof_2019_path / "ASVspoof2019_LA_cm_protocols" / "ASVspoof2019.LA.cm.dev.trl.txt")
+    paths["eval_meta_path"] = (ASVspoof_2021_path / f"ASVspoof2021_{track}_eval" / f"ASVspoof2021.{track}.cm.eval.trl.txt")
+
+    # ASV score path
+    paths['asv_score_dev_path'] = (database_path / "ASVspoof2019_LA_asv_scores" / "ASVspoof2019.LA.asv.dev.gi.trl.scores.txt")
+    paths['asv_keys_path'] = (ASVspoof_2021_path / "keys")
+
+    return paths
+
+
+def get_loader(
+        seed: int,
+        trn_meta_path: Path,
+        dev_meta_path: Path,
+        eval_meta_path: Path,
+        trn_flac_path: Path,
+        dev_flac_path: Path,
+        eval_flac_path: Path,
+        config: dict) -> List[torch.utils.data.DataLoader]:
+    """Make PyTorch DataLoaders for train / developement / evaluation"""
+
+    d_label_trn, file_train = genSpoof_list(dir_meta=trn_meta_path,
+                                            is_train=True,
+                                            is_eval=False)
+    print("no. training files:", len(file_train))
+
+    train_set = Dataset_ASVspoof2019_train(list_IDs=file_train,
+                                           labels=d_label_trn,
+                                           base_dir=trn_flac_path)
+    gen = torch.Generator()
+    gen.manual_seed(seed)
+    trn_loader = DataLoader(train_set,
+                            batch_size=config["batch_size"],
+                            shuffle=True,
+                            drop_last=True,
+                            pin_memory=True,
+                            worker_init_fn=seed_worker,
+                            generator=gen)
+
+    _, file_dev = genSpoof_list(dir_meta=dev_meta_path,
+                                is_train=False,
+                                is_eval=False)
+    print("no. validation files:", len(file_dev))
+
+    dev_set = Dataset_ASVspoof2019_dev(list_IDs=file_dev,
+                                            base_dir=dev_flac_path)
+    dev_loader = DataLoader(dev_set,
+                            batch_size=config["batch_size"],
+                            shuffle=False,
+                            drop_last=False,
+                            pin_memory=True)
+
+    file_eval = genSpoof_list(dir_meta=eval_meta_path,
+                              is_train=False,
+                              is_eval=True)
+    eval_set = Dataset_ASVspoof2021_eval(list_IDs=file_eval,
+                                             base_dir=eval_flac_path)
+    eval_loader = DataLoader(eval_set,
+                             batch_size=config["batch_size"],
+                             shuffle=False,
+                             drop_last=False,
+                             pin_memory=True)
+
+    return trn_loader, dev_loader, eval_loader
+
+
+def genSpoof_list( dir_meta,is_train=False,is_eval=False):
+    
+    d_meta = {}
+    file_list=[]
+    with open(dir_meta, 'r') as f:
+         l_meta = f.readlines()
+
+    if (is_train):
+        for line in l_meta:
+             _,key,_,_,label = line.strip().split()
+             
+             file_list.append(key)
+             d_meta[key] = 1 if label == 'bonafide' else 0
+        return d_meta,file_list
+    
+    elif(is_eval):
+        for line in l_meta:
+            key= line.strip()
+            file_list.append(key)
+        return file_list
+    else:
+        for line in l_meta:
+             _,key,_,_,label = line.strip().split()
+             
+             file_list.append(key)
+             d_meta[key] = 1 if label == 'bonafide' else 0
+        return d_meta,file_list
+
+
+def pad(x, max_len=64600):
+    x_len = x.shape[0]
+    if x_len >= max_len:
+        return x[:max_len]
+    # need to pad
+    num_repeats = int(max_len / x_len) + 1
+    padded_x = np.tile(x, (1, num_repeats))[:, :max_len][0]
+    return padded_x
+
+
+def pad_random(x: np.ndarray, max_len: int = 64600):
+    x_len = x.shape[0]
+    # if duration is already long enough
+    if x_len >= max_len:
+        stt = np.random.randint(x_len - max_len)
+        return x[stt:stt + max_len]
+
+    # if too short
+    num_repeats = int(max_len / x_len) + 1
+    padded_x = np.tile(x, (num_repeats))[:max_len]
+    return padded_x
+
+
+class Dataset_ASVspoof2019_train(Dataset):
+    def __init__(self, list_IDs, labels, base_dir):
+        """self.list_IDs	: list of strings (each string: utt key),
+           self.labels      : dictionary (key: utt key, value: label integer)"""
+        self.list_IDs = list_IDs
+        self.labels = labels
+        self.base_dir = base_dir
+        self.cut = 64600  # take ~4 sec audio (64600 samples)
+
+    def __len__(self):
+        return len(self.list_IDs)
+
+    def __getitem__(self, index):
+        key = self.list_IDs[index]
+        X, _ = sf.read(str(self.base_dir / f"flac/{key}.flac"))
+        X_pad = pad_random(X, self.cut)
+        x_inp = Tensor(X_pad)
+        y = self.labels[key]
+        return x_inp, y
+
+
+class Dataset_ASVspoof2019_dev(Dataset):
+    def __init__(self, list_IDs, base_dir):
+        """self.list_IDs	: list of strings (each string: utt key),
+        """
+        self.list_IDs = list_IDs
+        self.base_dir = base_dir
+        self.cut = 64600  # take ~4 sec audio (64600 samples)
+
+    def __len__(self):
+        return len(self.list_IDs)
+
+    def __getitem__(self, index):
+        key = self.list_IDs[index]
+        X, _ = sf.read(str(self.base_dir / f"flac/{key}.flac"))
+        X_pad = pad(X, self.cut)
+        x_inp = Tensor(X_pad)
+        return x_inp, key
+
+
+class Dataset_ASVspoof2021_eval(Dataset):
+    def __init__(self, list_IDs, base_dir):
+            '''self.list_IDs	: list of strings (each string: utt key),
+               '''
+               
+            self.list_IDs = list_IDs
+            self.base_dir = base_dir
+            self.cut=64600 # take ~4 sec audio (64600 samples)
+
+    def __len__(self):
+            return len(self.list_IDs)
+
+    def __getitem__(self, index):
+            
+            key = self.list_IDs[index]
+            X, fs = sf.read(str(self.base_dir)+'/flac/'+key+'.flac')
+            X_pad = pad(X,self.cut)
+            x_inp = Tensor(X_pad)
+            return x_inp, key
---
+++ main_2021.py
@@ -0,0 +1,252 @@
+"""
+Main script that trains, validates, and evaluates
+various models including AASIST.
+
+AASIST
+Copyright (c) 2021-present NAVER Corp.
+MIT license
+"""
+import argparse
+import json
+import os
+import sys
+import warnings
+from pathlib import Path
+from shutil import copy
+import io
+import contextlib
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from torchcontrib.optim import SWA
+
+from evaluation import calculate_tDCF_EER
+from utils import create_optimizer, seed_worker, set_seed, str_to_bool
+from common.models import get_model
+from common.training import train_epoch
+from data_loaders.loader_2021 import get_database_path, get_loader
+from evaluate_package.evaluation_file_2021 import produce_evaluation_file
+from evaluate_package.ASVspoof2021.main import evaluation_API
+
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+
+def main(args: argparse.Namespace) -> None:
+    """
+    Main function.
+    Trains, validates, and evaluates the ASVspoof detection model.
+    """
+    # load experiment configurations
+    with open(args.config, "r") as f_json:
+        config = json.loads(f_json.read())
+    model_config = config["model_config"]
+    optim_config = config["optim_config"]
+    optim_config["epochs"] = config["num_epochs"]
+    track = args.track
+    assert track in ["LA", "PA", "DF"], "Invalid track given"
+    if "eval_all_best" not in config:
+        config["eval_all_best"] = "True"
+    if "freq_aug" not in config:
+        config["freq_aug"] = "False"
+
+    # make experiment reproducible
+    set_seed(args.seed, config)
+
+    # define database related paths
+    output_dir = Path(args.output_dir)
+    database_path = Path('DB')
+
+    # define model related paths
+    model_tag = "{}_{}_{}".format(
+        track,
+        2021,
+        os.path.splitext(os.path.basename(args.config))[0])
+    if args.comment:
+        model_tag = model_tag + "_{}".format(args.comment)
+    model_tag = output_dir / model_tag
+    model_save_path = model_tag / "weights"
+    eval_score_path = model_tag / config["eval_output"]
+    writer = SummaryWriter(model_tag)
+    os.makedirs(model_save_path, exist_ok=True)
+    copy(args.config, model_tag / "config.conf")
+
+    # set device
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print("Device: {}".format(device))
+    if device == "cpu":
+        raise ValueError("GPU not detected!")
+
+    # define model architecture
+    model = get_model(model_config, device)
+
+    # define dataset path
+    paths = get_database_path(database_path, track=track)
+
+    # define dataloaders
+    trn_loader, dev_loader, eval_loader = get_loader(
+        seed = args.seed,
+        trn_meta_path=paths['trn_meta_path'],
+        dev_meta_path=paths['dev_meta_path'],
+        eval_meta_path=paths['eval_meta_path'],
+        trn_flac_path=paths['trn_flac_path'],
+        dev_flac_path=paths['dev_flac_path'],
+        eval_flac_path=paths['eval_flac_path'],
+        config=config)
+
+    # evaluates pretrained model and exit script
+    if args.eval:
+        model.load_state_dict(
+            torch.load(config["model_path"], map_location=device))
+        print("Model loaded : {}".format(config["model_path"]))
+        print("Start evaluation...")
+        produce_evaluation_file(eval_loader, model, device,
+                                eval_score_path, paths["eval_meta_path"])
+        _buf = io.StringIO()
+        with contextlib.redirect_stdout(_buf):
+            eval_tdcf, eval_eer = evaluation_API(
+                    cm_score_file=eval_score_path,
+                    track=args.track,
+                    label_dir=paths['asv_keys_path'],
+                )
+        table_text = _buf.getvalue()
+        with open(model_tag / "t-DCF_EER.txt", "w") as fh:
+            fh.write(table_text)
+        print("DONE.")
+        sys.exit(0)
+
+    # get optimizer and scheduler
+    optim_config["steps_per_epoch"] = len(trn_loader)
+    optimizer, scheduler = create_optimizer(model.parameters(), optim_config)
+    optimizer_swa = SWA(optimizer)
+
+    best_dev_eer = 1.
+    best_eval_eer = 100.
+    best_dev_tdcf = 0.05
+    best_eval_tdcf = 1.
+    n_swa_update = 0  # number of snapshots of model to use in SWA
+    f_log = open(model_tag / "metric_log.txt", "a")
+    f_log.write("=" * 5 + "\n")
+
+    # make directory for metric logging
+    metric_path = model_tag / "metrics"
+    os.makedirs(metric_path, exist_ok=True)
+
+    # Training
+    for epoch in range(config["num_epochs"]):
+        print("Start training epoch{:03d}".format(epoch))
+        running_loss = train_epoch(trn_loader, model, optimizer, device,
+                                   scheduler, config)
+        produce_evaluation_file(dev_loader, model, device,
+                                metric_path/"dev_score.txt", paths["dev_meta_path"])
+        dev_eer, dev_tdcf = calculate_tDCF_EER(
+            cm_scores_file=metric_path/"dev_score.txt",
+            asv_score_file=paths['asv_score_dev_path'],
+            output_file=metric_path/"dev_t-DCF_EER_{}epo.txt".format(epoch),
+            printout=False)
+        print("DONE.\nLoss:{:.5f}, dev_eer: {:.3f}, dev_tdcf:{:.5f}".format(
+            running_loss, dev_eer, dev_tdcf))
+        writer.add_scalar("loss", running_loss, epoch)
+        writer.add_scalar("dev_eer", dev_eer, epoch)
+        writer.add_scalar("dev_tdcf", dev_tdcf, epoch)
+
+        best_dev_tdcf = min(dev_tdcf, best_dev_tdcf)
+        if best_dev_eer >= dev_eer:
+            print("best model find at epoch", epoch)
+            best_dev_eer = dev_eer
+            torch.save(model.state_dict(),
+                       model_save_path / "epoch_{}_{:03.3f}.pth".format(epoch, dev_eer))
+
+            # do evaluation whenever best model is renewed
+            if str_to_bool(config["eval_all_best"]):
+                produce_evaluation_file(eval_loader, model, device,
+                                        eval_score_path, paths["eval_meta_path"])
+                eval_eer, eval_tdcf = calculate_tDCF_EER(
+                    cm_scores_file=eval_score_path,
+                    asv_score_file=paths['asv_score_eval_path'],
+                    output_file=metric_path /
+                    "t-DCF_EER_{:03d}epo.txt".format(epoch))
+
+                log_text = "epoch{:03d}, ".format(epoch)
+                if eval_eer < best_eval_eer:
+                    log_text += "best eer, {:.4f}%".format(eval_eer)
+                    best_eval_eer = eval_eer
+                if eval_tdcf < best_eval_tdcf:
+                    log_text += "best tdcf, {:.4f}".format(eval_tdcf)
+                    best_eval_tdcf = eval_tdcf
+                    torch.save(model.state_dict(),
+                               model_save_path / "best.pth")
+                if len(log_text) > 0:
+                    print(log_text)
+                    f_log.write(log_text + "\n")
+
+            print("Saving epoch {} for swa".format(epoch))
+            optimizer_swa.update_swa()
+            n_swa_update += 1
+        writer.add_scalar("best_dev_eer", best_dev_eer, epoch)
+        writer.add_scalar("best_dev_tdcf", best_dev_tdcf, epoch)
+
+    print("Start final evaluation")
+    epoch += 1
+    if n_swa_update > 0:
+        optimizer_swa.swap_swa_sgd()
+        optimizer_swa.bn_update(trn_loader, model, device=device)
+    produce_evaluation_file(eval_loader, model, device, eval_score_path,
+                            paths["eval_meta_path"])
+    eval_eer, eval_tdcf = calculate_tDCF_EER(cm_scores_file=eval_score_path,
+                                             asv_score_file=paths['asv_score_eval_path'],
+                                             output_file=model_tag / "t-DCF_EER.txt")
+    f_log = open(model_tag / "metric_log.txt", "a")
+    f_log.write("=" * 5 + "\n")
+    f_log.write("EER: {:.3f}, min t-DCF: {:.5f}".format(eval_eer, eval_tdcf))
+    f_log.close()
+
+    torch.save(model.state_dict(),
+               model_save_path / "swa.pth")
+
+    if eval_eer <= best_eval_eer:
+        best_eval_eer = eval_eer
+    if eval_tdcf <= best_eval_tdcf:
+        best_eval_tdcf = eval_tdcf
+        torch.save(model.state_dict(),
+                   model_save_path / "best.pth")
+    print("Exp FIN. EER: {:.3f}, min t-DCF: {:.5f}".format(
+        best_eval_eer, best_eval_tdcf))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="ASVspoof detection system")
+    parser.add_argument("--config",
+                        dest="config",
+                        type=str,
+                        help="configuration file",
+                        required=True)
+    parser.add_argument(
+        "--output_dir",
+        dest="output_dir",
+        type=str,
+        help="output directory for results",
+        default="./exp_result",
+    )
+    parser.add_argument("--seed",
+                        type=int,
+                        default=1234,
+                        help="random seed (default: 1234)")
+    parser.add_argument(
+        "--eval",
+        action="store_true",
+        help="when this flag is given, evaluates given model and exit")
+    parser.add_argument("--comment",
+                        type=str,
+                        default=None,
+                        help="comment to describe the saved model")
+    parser.add_argument("--eval_model_weights",
+                        type=str,
+                        default=None,
+                        help="directory to the model weight file (can be also given in the config file)")
+    parser.add_argument("--track",
+                        type=str,
+                        default="LA",
+                        help="specify the track of ASVspoof (LA/DF)")
+    main(parser.parse_args())
