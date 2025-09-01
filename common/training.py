8956d439dd7d368abf8725c2c8b64ecc4adfa045
Impress_kdh <d0_0h@inu.ac.kr>
2025-09-01 17:48:50 +0900

    Add: ASVspoof 2019 version

---
+++ common/training.py
@@ -0,0 +1,49 @@
+import argparse
+from typing import Union
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+from utils import str_to_bool
+
+
+def train_epoch(
+    trn_loader: DataLoader,
+    model,
+    optim: Union[torch.optim.SGD, torch.optim.Adam],
+    device: torch.device,
+    scheduler: torch.optim.lr_scheduler,
+    config: argparse.Namespace):
+    """Train the model for one epoch"""
+    running_loss = 0
+    num_total = 0.0
+    ii = 0
+    model.train()
+
+    # set objective (Loss) functions
+    weight = torch.FloatTensor([0.1, 0.9]).to(device)
+    criterion = nn.CrossEntropyLoss(weight=weight)
+    for batch_x, batch_y in tqdm(trn_loader, desc="Training", leave=False):
+        batch_size = batch_x.size(0)
+        num_total += batch_size
+        ii += 1
+        batch_x = batch_x.to(device)
+        batch_y = batch_y.view(-1).type(torch.int64).to(device)
+        _, batch_out = model(batch_x, Freq_aug=str_to_bool(config["freq_aug"]))
+        batch_loss = criterion(batch_out, batch_y)
+        running_loss += batch_loss.item() * batch_size
+        optim.zero_grad()
+        batch_loss.backward()
+        optim.step()
+
+        if config["optim_config"]["scheduler"] in ["cosine", "keras_decay"]:
+            scheduler.step()
+        elif scheduler is None:
+            pass
+        else:
+            raise ValueError("scheduler error, got:{}".format(scheduler))
+
+    running_loss /= num_total
+    return running_loss
---
+++ evaluate_package/evaluation_file_2019.py
@@ -0,0 +1,33 @@
+import torch
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+
+def produce_evaluation_file(
+    data_loader: DataLoader,
+    model,
+    device: torch.device,
+    save_path: str,
+    trial_path: str) -> None:
+    """Perform evaluation and save the score to a file"""
+    model.eval()
+    with open(trial_path, "r") as f_trl:
+        trial_lines = f_trl.readlines()
+    fname_list = []
+    score_list = []
+    for batch_x, utt_id in tqdm(data_loader, desc="Evaluating", leave=False):
+        batch_x = batch_x.to(device)
+        with torch.no_grad():
+            _, batch_out = model(batch_x)
+            batch_score = (batch_out[:, 1]).data.cpu().numpy().ravel()
+        # add outputs
+        fname_list.extend(utt_id)
+        score_list.extend(batch_score.tolist())
+
+    assert len(trial_lines) == len(fname_list) == len(score_list)
+    with open(save_path, "w") as fh:
+        for fn, sco, trl in zip(fname_list, score_list, trial_lines):
+            _, utt_id, _, src, key = trl.strip().split(' ')
+            assert fn == utt_id
+            fh.write("{} {} {} {}\n".format(utt_id, src, key, sco))
+    print("Scores saved to {}".format(save_path))
